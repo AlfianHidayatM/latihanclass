87363ee461d56d7dac159e6c678637b04470ece0
AlfianHidayatM <32305314+AlfianHidayatM@users.noreply.github.com>
2018-01-20 08:39:27 +0700

    Membuat data yang di Input terlebih dahulu

--- classcomputer.cpp
+++ classcomputer.cpp
@@ -16,3 +16,15 @@ class Computer {
 	int RAM;
 	double proc_speed;
 };
+
+int main()
+{
+	Computer computer_one;
+	computer_one.name = "Work-a-tron 5000";
+	computer_one.RAM = 4;
+	computer_one.proc_speed = 2.6;
+
+	Computer computer_two;
+	computer_two.name = "1080p-Video-Renderer 9001";
+	computer_two.RAM = 8;
+	computer_two.proc_speed = 3;
